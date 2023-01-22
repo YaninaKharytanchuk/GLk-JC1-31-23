b98c8cf2f592c092c556c8cc7169d4859fda2fbf
YaninaKharytanchuk <yanina100192@mail.ru>
2023-01-22 16:40:55 +0300

    hello

---
+++ src/Task2/House.java
@@ -0,0 +1,24 @@
+package Task2;
+
+public class House {
+    public static void main(String[] args) {
+        int i = 5; // Приведение int в byte
+        byte b = (byte) i; // Приведение byte в char
+        char g = (char) b;
+        long o = 8888; // Приведение long в float
+        float k = o;
+        boolean values = true;
+        char number = 'f'; // Приведение char в int
+        int a = number;
+        byte w = 8;
+        short d = (byte) w;
+        float z = 1.111f; // Приведение float в double
+        double j = (float) z;
+        int max = Math.max(10,5);
+        int min = Math.min (10,5);
+
+        System.out.println(min);
+        System.out.println(g);
+    }
+
+}
---
+++ src/Task2/Room.java
@@ -0,0 +1,137 @@
+package Task2;
+
+public class Room {
+  private int height;
+    private Integer Integer;
+    private byte door;
+    private Byte Byte;
+    private short width;
+    private Short Short;
+    private  long chairs;
+    private Long Long;
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public java.lang.Integer getInteger() {
+    return Integer;
+  }
+
+  public void setInteger(java.lang.Integer integer) {
+    Integer = integer;
+  }
+
+  public byte getDoor() {
+    return door;
+  }
+
+  public void setDoor(byte door) {
+    this.door = door;
+  }
+
+  public java.lang.Byte getByte() {
+    return Byte;
+  }
+
+  public void setByte(java.lang.Byte aByte) {
+    Byte = aByte;
+  }
+
+  public short getWidth() {
+    return width;
+  }
+
+  public void setWidth(short width) {
+    this.width = width;
+  }
+
+  public java.lang.Short getShort() {
+    return Short;
+  }
+
+  public void setShort(java.lang.Short aShort) {
+    Short = aShort;
+  }
+
+  public long getChairs() {
+    return chairs;
+  }
+
+  public void setChairs(long chairs) {
+    this.chairs = chairs;
+  }
+
+  public java.lang.Long getLong() {
+    return Long;
+  }
+
+  public void setLong(java.lang.Long aLong) {
+    Long = aLong;
+  }
+
+  public float getTemperature() {
+    return temperature;
+  }
+
+  public void setTemperature(float temperature) {
+    this.temperature = temperature;
+  }
+
+  public java.lang.Float getFloat() {
+    return Float;
+  }
+
+  public void setFloat(java.lang.Float aFloat) {
+    Float = aFloat;
+  }
+
+  public double getHumidity() {
+    return humidity;
+  }
+
+  public void setHumidity(double humidity) {
+    this.humidity = humidity;
+  }
+
+  public char getVolume() {
+    return volume;
+  }
+
+  public void setVolume(char volume) {
+    this.volume = volume;
+  }
+
+  public boolean isWindow() {
+    return window;
+  }
+
+  public void setWindow(boolean window) {
+    this.window = window;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  private  float temperature;
+    private Float Float;
+    private double humidity;
+    private char volume;
+    private boolean window;
+    String name;
+
+    public void say() {
+        boolean isExsiste = true;
+        int door = 7;
+        System.out.println(name);
+    }
+}
